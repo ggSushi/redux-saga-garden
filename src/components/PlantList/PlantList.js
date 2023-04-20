eca1d9c38b1674ed524436a6ba77650d912259c4
Glonel Dimapilis <gdimapilis@aol.com>
2023-04-19 21:54:24 -0500

    Thanks, Chris! Still need to wrap my head around it all, but we added an action.type to plantList and changed some of the action.type names to make more technical sense.

--- src/components/PlantList/PlantList.js
+++ src/components/PlantList/PlantList.js
@@ -3,19 +3,36 @@ import { useDispatch, useSelector} from 'react-redux';
 
 
 function PlantList() {
+        // const reduxState = useSelector(store => store);
     const dispatch = useDispatch();
+    const plantList = useSelector(store => store.plantList);
 
-    const reduxState = useSelector(store => store);
+    const getPlants = () => {
+        dispatch({ type: 'FETCH_PLANT_LIST'})
+    }
 
     useEffect(() => {
         console.log('component did mount');
         // dispatch an action to request the plantList from the API
+        getPlants();
+        console.log(`YO`, plantList)
     }, []); 
 
     return (
         <div>
             <h3>This is the plant list</h3>
-            <pre>{JSON.stringify(reduxState)}</pre>
+            {/* Can't get this to work just yet! Oops, my bad hahahaha */}
+            <h5></h5>
+            <pre>{JSON.stringify(plantList)}</pre>
+            <ul>
+                {
+                    plantList.map(plant => (
+                        <li key={plant.id}>
+                            {plant.name}
+                        </li>
+                    ))
+                }
+            </ul>
         </div>
     );
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-
+import axios from 'axios';
+import createSagaMiddleware from 'redux-saga';
+import { takeEvery, put } from 'redux-saga/effects';
+import logger from 'redux-logger';
 import App from './App';
 
 // this startingPlantArray should eventually be removed
@@ -16,14 +19,45 @@ const plantList = (state = startingPlantArray, action) => {
   switch (action.type) {
     case 'ADD_PLANT':
       return [ ...state, action.payload ]
+    case 'SET_PLANTS':
+      return action.payload;
     default:
       return state;
   }
 };
 
+// TODO Write code for Sagas
+
+function* fetchPlantList() {
+  try {
+    const plants = yield axios.get('/api/plant');
+    yield put({ type: 'SET_PLANTS', payload: plants.data})
+  } catch (error ){
+    console.log(`Error in fetchPlantList ${error}`);
+    alert(`Something went wrong, dude`);
+  }
+}
+
+
+
+// createSagaMiddleware
+const sagaMiddleware = createSagaMiddleware();
+
+// rootSaga
+function* rootSaga() {
+  // Sagas go here
+  yield takeEvery('FETCH_PLANT_LIST', fetchPlantList)
+}
+
 const store = createStore(
-  combineReducers({ plantList }),
+  combineReducers({ 
+    plantList
+   }),
+   applyMiddleware(sagaMiddleware, logger)
 );
+
+// sagaMiddleware
+sagaMiddleware.run(rootSaga);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
